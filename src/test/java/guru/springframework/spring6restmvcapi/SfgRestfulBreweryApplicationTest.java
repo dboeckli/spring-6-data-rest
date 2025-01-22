0a70b65b8fd8a5af19d09c65fc6f6c54e402dee8
adm-boeckli <dboeckli@gmail.com>
2025-01-22 11:15:41 +0100

    refactor project and build

--- src/test/java/guru/springframework/spring6restmvcapi/SfgRestfulBreweryApplicationTest.java
+++ src/test/java/guru/springframework/spring6restmvcapi/SfgRestfulBreweryApplicationTest.java
@@ -1,13 +1,16 @@
 package guru.springframework.spring6restmvcapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+@Slf4j
 public class SfgRestfulBreweryApplicationTest {
 
     @Test
     public void contextLoads() {
+        log.info("Testing Spring 6 Application...");
     }
 
 }
