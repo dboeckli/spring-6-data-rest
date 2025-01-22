09bd392d3b1686c63182880181195b951b3a5bd8
adm-boeckli <dboeckli@gmail.com>
2025-01-22 11:33:27 +0100

    test bootstrap

--- src/test/java/guru/springframework/spring6restmvcapi/SfgRestfulBreweryApplicationTest.java
+++ src/test/java/guru/springframework/spring6restmvcapi/SfgRestfulBreweryApplicationTest.java
@@ -1,16 +1,24 @@
 package guru.springframework.spring6restmvcapi;
 
+import guru.springframework.spring6restmvcapi.repository.BeerRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 @Slf4j
 public class SfgRestfulBreweryApplicationTest {
+    
+    @Autowired
+    BeerRepository beerRepository;
 
     @Test
     public void contextLoads() {
         log.info("Testing Spring 6 Application...");
+        assertEquals(30, beerRepository.count()); // Should not throw any exception
     }
 
 }
